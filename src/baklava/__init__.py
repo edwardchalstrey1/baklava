4b552a22753d9f17a078a011cc56a641d3465adb
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-09-03 14:01:46 +0100

    Add all files

---
+++ src/baklava/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about baklava."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Edward Chalstrey"
+__email__ = "edwardchalstrey@gmail.com"
+__version__ = "0.1.0"
